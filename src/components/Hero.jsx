53051d34aef0fc98f3ee466ce1672c3c453f24e6
Abdul-Muqaddam <chandansar444999@gmail.com>
2024-03-10 11:47:44 +0500

    hero section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
-const Hero = () => {
+import { motion } from "framer-motion"
+import { styles } from "./styles"
+import { ComputersCanvas } from "./canvas"
+const Hero = ({isMobile}) => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-[900px] mx-auto" >
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center top-5 w-5 rounded-full" />
+        <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        <div >
+          <h1 className={`${styles.heroHeadText} text-white `}>Hi i'm<span className="text-[#915eff]"> Abdul-Muqaddam</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I developed 3d visuals user <br className="sm:block hidden" /> interface and web application </p>
+        </div>
+      </div>
+      <ComputersCanvas/>
+      <div className={`absolute ${isMobile?"bottom-0":"bottom-12"} w-full flex justify-center items-center`}>
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 ">
+            <motion.div animate={{y:[0,24,0]}} transition={{duration:1.5,repeat:Infinity,repeatType:'loop'}} className="w-3 h-3 rounded-full bg-secondary ">
+
+            </motion.div>
+          </div>
+        </a>
+      </div>
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,11 +13,11 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex"> Abdul-Muqaddam <span className="sm:block hidden"> &nbsp;| Frontend developer</span></p>
         </Link>
-        <ul className="list-none hidden sm:flex flex-row gap-10">{navLinks.map((Link)=>(<li key={Link.id} className={`${active===Link.title?"text-white":"text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer` } onclick={()=>setActive(Link.title)}><a href={`${Link.id}`}>{Link.title}</a></li>))}</ul>
+        <ul className="list-none hidden sm:flex flex-row gap-10">{navLinks.map((Link)=>(<li key={Link.id} className={`${active===Link.title?"text-white":"text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer` } onClick={()=>setActive(Link.title)}><a href={`${Link.id}`}>{Link.title}</a></li>))}</ul>
         <div className="sm:hidden flex flex-1 justify-end items-center">
           <img src={toggle?close:menu} alt="" className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={()=>settoggle(!toggle)} />
           <div className={`${!toggle?"hidden":"flex"} p-6 black-gradient absolute top-20 right-0 mx-5 my-2 min-w-[140px] z-10 rounded-xl`}>
-          <ul className="list-none flex justify-end items-start flex-col gap-4">{navLinks.map((Link)=>(<li key={Link.id} className={`${active===Link.title?"text-white":"text-secondary"} font-poppins font-medium cursor-pointer text-[16px]`  } onclick={()=>{settoggle(!toggle); setActive(Link.title)}}><a href={`${Link.id}`}>{Link.title}</a></li>))}</ul>
+          <ul className="list-none flex justify-end items-start flex-col gap-4">{navLinks.map((Link)=>(<li key={Link.id} className={`${active===Link.title?"text-white":"text-secondary"} font-poppins font-medium cursor-pointer text-[16px]`  } onClick={()=>{settoggle(!toggle); setActive(Link.title)}}><a href={`${Link.id}`}>{Link.title}</a></li>))}</ul>
           </div>
         </div>
       </div>
